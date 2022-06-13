2277b9ac8666d217df032e5423d4c13ae81b9816
Justus Pfaue <s6jupfau@uni-bonn.de>
2022-06-13 19:50:51 +0200

    finished Controller for Anmeldungsscreen

--- src/main/java/de/oose/webservice/client/gui/AnmeldungC.java
+++ src/main/java/de/oose/webservice/client/gui/AnmeldungC.java
@@ -1,6 +1,7 @@
 package de.oose.webservice.client.gui;
 
 import de.oose.webservice.client.Main;
+import de.oose.webservice.client.RestAPIClient;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -30,32 +31,39 @@ public class AnmeldungC {
         private Button register;
 
 
-        public void logIntoSystem(ActionEvent actionEvent) throws IOException {
+        public void registerIntoSystem(ActionEvent actionEvent) throws IOException {
                 boolean accepted = true;
-//                if  (name.getText().isEmpty() && password.getText().isEmpty())
-//                        exception.setText("Please enter your data!");
-//                else { //TODO HTTP REQUEST an die Datenbank
-//                        Object[] sender = new HttpRequestSender().sendGetRequest(.apiUrl + "/session/login?username=" + name.getText() + "&password=" + password.getText());
-//                        if ((Integer) sender[0] == HttpURLConnection.HTTP_ACCEPTED) {
-                        if (accepted/*TODO*/){
-                                //Loads the next scene
+                if (name.getText().isEmpty() && password.getText().isEmpty()) {
+                        exception.setText("Please enter your data!");
+                }
+                else if (RestAPIClient.register(name.getText(), password.getText())) {
+                        //Loads the next scene
+                        if (RestAPIClient.login(name.getText(), password.getText())) {
                                 Stage stage = (Stage) name.getScene().getWindow();
                                 FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Menu.fxml"));
                                 Scene scene = new Scene(fxmlLoader.load(), 960, 540);
                                 stage.setScene(scene);
                                 stage.show();
-                        } else {
-                                //Error Message
-                                exception.setText("ERROR: Username or Password wrong!");
                         }
+                } else {
+                        //Error Message
+                        exception.setText("ERROR: Username or Password wrong!");
                 }
-
-//        }
-
-        public void registerIntoSystem(ActionEvent actionEvent) throws IOException {
-                //TODO Justus send to Server, save and print text, registration succsess and then log in try
-                exception.setText("Thanks for choosing Armin ;) Try to LogIn!");
         }
 
-
+        public void logIntoSystem() {
+                if (RestAPIClient.login(name.getText(), password.getText())) {
+                        Stage stage = (Stage) name.getScene().getWindow();
+                        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Menu.fxml"));
+                        Scene scene = null;
+                        try {
+                                scene = new Scene(fxmlLoader.load(), 960, 540);
+                        } catch (IOException e) {
+                                throw new RuntimeException(e);
+                        }
+                        stage.setScene(scene);
+                        stage.show();
+                }
+                exception.setText("Thanks for choosing Armin ;) Try to LogIn!");
+        }
 }
