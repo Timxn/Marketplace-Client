077460404ef4bd1f2e668f5905b4706bf7d508f9
Justus Pfaue <s6jupfau@uni-bonn.de>
2022-06-14 11:26:52 +0200

    create empty unit tests

---
+++ src/test/java/de/oose/webservice/client/RestAPIClientTest.java
@@ -0,0 +1,48 @@
+package de.oose.webservice.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestAPIClientTest {
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void getBalance() {
+    }
+
+    @Test
+    void setMoney() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void getProducts() {
+    }
+
+    @Test
+    void getDepot() {
+    }
+
+    @Test
+    void buy() {
+    }
+
+    @Test
+    void addProductToMarket() {
+    }
+
+    @Test
+    void sell() {
+    }
+}
